a38f89a5c087a8469042cd97ae46beb85279dd57
张丹峰 <3310816449@qq.com>
2017-04-20 15:08:15 +0800

    单链表的倒数第k个节点

---
+++ src/InterView/Algotrithm/FindNumsAppearOnceSoluction.java
@@ -0,0 +1,23 @@
+package InterView.Algotrithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/4/18.
+ */
+public class FindNumsAppearOnceSoluction {
+    /**
+     * 一个整型数组里除了两个数字之外，其他的数字都出现了两次。请写程序找出这两个只出现一次的数字。
+     * @param array  2,5,6,6,5,1,2,3
+     * @param num1
+     * @param num2
+     * //num1,num2分别为长度为1的数组。传出参数
+    //将num1[0],num2[0]设置为返回结果
+     */
+    public void FindNumsAppearOnce(int [] array,int num1[] , int num2[]) {
+           Map hashMap=new HashMap();
+
+    }
+
+}
